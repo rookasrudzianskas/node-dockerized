8cb5193426cfd2a1e80bb6b4bbc64088bc8d1e75
Rokas <rookas.rudzenskas@gmail.com>
2022-11-21 19:10:51 +0200

    Rokas

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const app = express();
 const mongoose = require('mongoose');
 const port = process.env.PORT || 3000;
 
-mongoose.connect('mongodb://rokas:rokas@');
+mongoose.connect('mongodb://rokas:rokas@192.168.32.2:27017/?authSource=admin').then(() => {
+    console.log('Connected to database :gr');
+})
 
 app.get('/', (req, res) => {
     res.send('Hello! Bro! 🚀');
