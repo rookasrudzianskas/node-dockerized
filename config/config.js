6dda5d60bb03418ec9240639ba715ae2f53c9aa9
Rokas <rookas.rudzenskas@gmail.com>
2022-11-21 20:40:28 +0200

    Rokas

--- config/config.js
+++ config/config.js
@@ -3,4 +3,4 @@ module.exports = {
     MONGO_PORT: process.env.MONGO_PORT || 27017,
     MONGO_USER: process.env.MONGO_USER,
     MONGO_PASSWORD: process.env.MONGO_PASSWORD,
-}
+};
--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const {MONGO_USER, MONGO_PASSWORD, MONGO_IP, MONGO_PORT} = require("./config/config");
 const port = process.env.PORT || 3000;
 
-mongoose.connect('mongodb://rokas:rokas@mongo:27017/?authSource=admin').then(() => {
+mongoose.connect(`mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`).then(() => {
     console.log('Connected to database 🍏');
 }).catch((e) => {
     console.log('Connection failed 🍎');
