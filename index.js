ffdd06b3d82a68a26af12ce58a17775d411161fe
Rokas <rookas.rudzenskas@gmail.com>
2022-11-21 15:19:38 +0200

    Rokas

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello Rokas! 🚀');
+    res.send('Hello World! 🚀');
 });
 
 app.listen(port, () => {
