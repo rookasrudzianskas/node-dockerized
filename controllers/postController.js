ee019d8dd67e87fd3f7db533bc57bed35e404460
Rokas <rookas.rudzenskas@gmail.com>
2022-11-22 09:14:13 +0200

    Add getOnePost controller
    This controller will be used to get a single post by its id.

--- controllers/postController.js
+++ controllers/postController.js
@@ -14,3 +14,17 @@ exports.getAllPosts = async (req, res, next) => {
         res.status(404).json({message: error.message});
     }
 }
+
+exports.getOnePost = async (req, res, next) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        res.status(200).json({
+            status: 'success',
+            data: {
+                post
+            }
+        });
+    } catch (error) {
+        res.status(404).json({message: error.message});
+    }
+}
