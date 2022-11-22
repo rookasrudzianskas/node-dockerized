002e6ce542af15e347d65604b9fa0150fa7b4f10
Rokas <rookas.rudzenskas@gmail.com>
2022-11-22 09:18:46 +0200

    Add create and update post functionality
    - Add createPost and updatePost functions to postController
    - Add createPost and updatePost routes to postRoutes

--- controllers/postController.js
+++ controllers/postController.js
@@ -28,3 +28,34 @@ exports.getOnePost = async (req, res, next) => {
         res.status(404).json({message: error.message});
     }
 }
+
+exports.createPost = async (req, res, next) => {
+    try {
+        const post = await Post.create(req.body);
+        res.status(201).json({
+            status: 'success',
+            data: {
+                post,
+            }
+        });
+    } catch (error) {
+        res.status(409).json({message: error.message});
+    }
+}
+
+exports.updatePost = async (req, res, next) => {
+    try {
+        const post = await Post.findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+            runValidators: true
+        });
+        res.status(200).json({
+            status: 'success',
+            data: {
+                post
+            }
+        });
+    } catch (error) {
+        res.status(404).json({message: error.message});
+    }
+}
