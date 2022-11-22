af39e07b46d675bb8acf23f0db906bb9b0c5a970
Rokas <rookas.rudzenskas@gmail.com>
2022-11-22 09:32:10 +0200

    Rokas

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ const mongoose = require('mongoose');
 const {MONGO_USER, MONGO_PASSWORD, MONGO_IP, MONGO_PORT} = require("./config/config");
 const port = process.env.PORT || 3000;
 
+const postRouter = require('./routes/postRoutes');
+
 const mongoURL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`;
+
 
 const connectWithRetry = () => {
     mongoose.connect(mongoURL, {
@@ -21,9 +24,14 @@ const connectWithRetry = () => {
 
 connectWithRetry();
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
     res.send('Hello! Bro! 🚀');
 });
+
+// if someone sends request, and it looks like localhost:3000/posts, then it will be handled by postRouter
+app.use("/api/v1/posts", postRouter);
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
